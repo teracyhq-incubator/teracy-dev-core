263f855f39b378b197af8d6ed3f30ff98687969e
hoatle <hoatle@teracy.com>
2018-08-12 16:01:14 +0700

    move init stuff to TeracyDevCore module

--- lib/teracy-dev-core.rb
+++ lib/teracy-dev-core.rb
@@ -1,3 +1,5 @@
+require 'teracy-dev'
+
 require_relative 'teracy-dev-core/config/networks'
 require_relative 'teracy-dev-core/config/plugins'
 require_relative 'teracy-dev-core/config/provisioners'
@@ -13,5 +15,19 @@ require_relative 'teracy-dev-core/processors/variables'
 
 
 module TeracyDevCore
+  def self.init
+    TeracyDev.register_processor(TeracyDevCore::Processors::Variables.new)
 
+    TeracyDev.register_configurator(TeracyDevCore::Config::Networks.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::Plugins.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::Provisioners.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::SSH.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::SyncedFolders.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::Vgrant.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::VirtualBoxProvider.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::VM.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::WinRM.new)
+    TeracyDev.register_configurator(TeracyDevCore::Config::WinSSH.new)
+  end
 end
+
