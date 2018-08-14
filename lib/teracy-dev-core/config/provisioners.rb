4be608e9b16807610898ab046ad5c96ecd0f2813
hoatle <hoatle@teracy.com>
2018-08-14 10:17:05 +0700

    clean up provisioners config

--- lib/teracy-dev-core/config/provisioners.rb
+++ lib/teracy-dev-core/config/provisioners.rb
@@ -1,6 +1,4 @@
-require 'teracy-dev/config/configurator'
-
-require_relative 'provision_settings'
+require 'teracy-dev'
 
 module TeracyDevCore
   module Config
@@ -10,7 +8,6 @@ module TeracyDevCore
       def configure_node(settings, config)
         provisioners_settings = settings['provisioners'] ||= []
         @logger.debug("configure_node: #{provisioners_settings}")
-
         provisioners_settings.each do |provisioner_settings|
           @logger.info("provisioner ignored: #{provisioner_settings}") if provisioner_settings['enabled'] != true
           next if provisioner_settings['enabled'] != true
@@ -25,13 +22,19 @@ module TeracyDevCore
             preserve_order = true
           end
 
+          options = provisioner_settings.dup
+
+          ["_id", "type", "enabled", "run", "preserve_order"].each do |key|
+            options.delete(key)
+          end
+
           if provisioner_settings['name'].nil?
             config.vm.provision "#{type}", run: run, preserve_order: preserve_order do |provision|
-              ProvisionSettings.configure(type, provision, provisioner_settings)
+              provision.set_options(options)
             end
           else
             config.vm.provision provisioner_settings['name'], type: type, run: run, preserve_order: preserve_order do |provision|
-              ProvisionSettings.configure(type, provision, provisioner_settings)
+              provision.set_options(options)
             end
           end
         end
