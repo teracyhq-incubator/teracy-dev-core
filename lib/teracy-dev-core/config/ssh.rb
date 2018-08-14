4e8128b33e0c6b9abbeeec0c7e91406d10d51ef1
hoatle <hoatle@teracy.com>
2018-08-14 11:46:42 +0700

    code clean up

--- lib/teracy-dev-core/config/ssh.rb
+++ lib/teracy-dev-core/config/ssh.rb
@@ -9,29 +9,7 @@ module TeracyDevCore
       def configure_node(settings, config)
         ssh_settings = settings['ssh'] ||= {}
         @logger.debug("configure_node: #{ssh_settings}")
-
-        config.ssh.username = ssh_settings['username'] if TeracyDev::Util.exist?(ssh_settings['username'])
-        config.ssh.password = ssh_settings['password'] if TeracyDev::Util.exist?(ssh_settings['password'])
-        config.ssh.host = ssh_settings['host'] if TeracyDev::Util.exist?(ssh_settings['host'])
-        config.ssh.port = ssh_settings['port'] if TeracyDev::Util.exist?(ssh_settings['port'])
-        config.ssh.guest_port = ssh_settings['guest_port'] if TeracyDev::Util.exist?(ssh_settings['guest_port'])
-        config.ssh.private_key_path = ssh_settings['private_key_path'] if TeracyDev::Util.exist?(ssh_settings['private_key_path'])
-        # FIXME: boolean check here
-        config.ssh.keys_only = ssh_settings['keys_only'] if TeracyDev::Util.exist?(ssh_settings['keys_only'])
-        config.ssh.verify_host_key = ssh_settings['verify_host_key'] if TeracyDev::Util.exist?(ssh_settings['verify_host_key'])
-        config.ssh.forward_agent = ssh_settings['forward_agent'] if TeracyDev::Util.exist?(ssh_settings['forward_agent'])
-        config.ssh.forward_x11 = ssh_settings['forward_x11'] if TeracyDev::Util.exist?(ssh_settings['forward_x11'])
-        config.ssh.forward_env = ssh_settings['forward_env'] if TeracyDev::Util.exist?(ssh_settings['forward_env'])
-        config.ssh.insert_key = ssh_settings['insert_key'] if TeracyDev::Util.exist?(ssh_settings['insert_key'])
-        config.ssh.proxy_command = ssh_settings['proxy_command'] if TeracyDev::Util.exist?(ssh_settings['proxy_command'])
-        config.ssh.pty = ssh_settings['pty'] if TeracyDev::Util.exist?(ssh_settings['pty'])
-        config.ssh.keep_alive = ssh_settings['keep_alive'] if TeracyDev::Util.exist?(ssh_settings['keep_alive'])
-        config.ssh.shell = ssh_settings['shell'] if TeracyDev::Util.exist?(ssh_settings['shell'])
-        config.ssh.export_command_template = ssh_settings['export_command_template'] if TeracyDev::Util.exist?(ssh_settings['export_command_template'])
-        config.ssh.sudo_command = ssh_settings['sudo_command'] if TeracyDev::Util.exist?(ssh_settings['sudo_command'])
-        config.ssh.compression = ssh_settings['compression'] if TeracyDev::Util.exist?(ssh_settings['compression'])
-        config.ssh.dsa_authentication = ssh_settings['dsa_authentication'] if TeracyDev::Util.exist?(ssh_settings['dsa_authentication'])
-        config.ssh.extra_args = ssh_settings['extra_args'] if TeracyDev::Util.exist?(ssh_settings['extra_args'])
+        config.ssh.set_options(ssh_settings)
       end
     end
   end
--- lib/teracy-dev-core/config/synced_folders.rb
+++ lib/teracy-dev-core/config/synced_folders.rb
@@ -10,46 +10,20 @@ module TeracyDevCore
         @logger.debug("configure_node: #{synced_folders_settings}")
 
         synced_folders_settings.each do |settings|
-          options = {}
-          host_os = Vagrant::Util::Platform.platform
-          host_os_type = ''
+          settings['type'] = settings['type'] ||= 'virtual_box'
+          host = settings['host']
+          guest = settings['guest']
 
-          case host_os
-          when /^(mswin|mingw).*/
-            host_os_type = 'windows'
-          when /^(linux|cygwin).*/
-            host_os_type = 'linux'
-          when /^(mac|darwin).*/
-            host_os_type = 'mac'
+          ['host', 'guest'].each do |key|
+            settings.delete(key)
           end
 
-          # options from http://docs.vagrantup.com/v2/synced-folders/basic_usage.html
-          options[:create] = settings['create'] unless settings['create'].nil?
-          options[:disabled] = settings['disabled'] unless settings['disabled'].nil?
-          options[:owner] = settings['owner'] unless settings['owner'].nil?
-          options[:group] = settings['group'] unless settings['group'].nil?
-          options[:mount_options] = settings['mount_options'] unless settings['mount_options'].nil?
-          options[:type] = settings['type'] unless settings['type'].nil? or settings['type'] == 'virtual_box'
-
-          case settings['type']
-          when 'nfs'
-            options[:nfs_export] = settings['nfs_export'] if !!settings['nfs_export'] == settings['nfs_export']
-            options[:nfs_udp] = settings['nfs_udp'] if !!settings['nfs_udp'] == settings['nfs_udp']
-            options[:nfs_version] = settings['nfs_version'] unless settings['nfs_version'].nil?
-          when 'rsync'
-            options[:rsync__args] = settings['rsync__args'] unless settings['rsync__args'].nil? or settings['rsync__args'].empty?
-            options[:rsync__auto] = settings['rsync__auto'] if !!settings['rsync__auto'] == settings['rsync__auto']
-            options[:rsync__chown] = settings['rsync__chown'] if !!settings['rsync__chown'] == settings['rsync__chown']
-            options[:rsync__exclude] = settings['rsync__exclude'] unless settings['rsync__exclude'].nil? or settings['rsync__exclude'].empty?
-          when 'smb'
-            options[:smb_host] = settings['smb_host'] unless settings['smb_host'].nil? or settings['smb_host'].empty?
-            options[:smb_password] = settings['smb_password'] unless settings['smb_password'].nil? or settings['smb_password'].empty?
-            options[:smb_username] = settings['smb_username'] unless settings['smb_password'].nil? or settings['smb_password'].empty?
+          settings.each do |key, val|
+            settings[key.to_sym] = val
+            settings.delete(key)
           end
 
-          if settings['supports'].nil? or settings['supports'].include?(host_os_type)
-            config.vm.synced_folder settings['host'], settings['guest'], options
-          end
+          config.vm.synced_folder host, guest, settings
         end
       end
     end
